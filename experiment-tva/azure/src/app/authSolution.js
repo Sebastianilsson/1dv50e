e6349e458b84e248fb0994db4e141970bb8bb291
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2020-05-07 10:59:59 +0200

    installing package needed to comunicate with azure ad b2c

--- experiment-tva/azure/src/app/authSolution.js
+++ experiment-tva/azure/src/app/authSolution.js
@@ -1,3 +1,15 @@
+import { UserAgentApplication } from "msal";
+
+const msalApp = new UserAgentApplication({
+  auth: {
+    clientId: "245e9392-c666-4d51-8f8a-bfd9e55b2456",
+    authority: "https://login.microsoftonline.com/common",
+    validateAuthority: true,
+    postLogoutRedirectUri: "http://localhost:3000",
+    navigateToLoginRequestUrl: false,
+  },
+});
+
 export const userLogin = (email, password) => {
   window.callToLogin = performance.now();
   console.log(`Hej ${email}, du har lösenord: ${password}`);
