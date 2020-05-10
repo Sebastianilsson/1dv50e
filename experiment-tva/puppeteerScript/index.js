2bf95bb206fe2c3928b1a94a30a24ee5b4467521
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2020-05-10 12:54:30 +0200

    updating script to nosql aws and firebase

--- experiment-tva/puppeteerScript/index.js
+++ experiment-tva/puppeteerScript/index.js
@@ -1,18 +1,23 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
-const jsonFileNames = ["baseResult.json", "firebaseResult.json"];
-let services = [
-  "http://localhost:3001/#/",
-  "http://localhost:3002/#/",
-  "http://localhost:3003/#/",
-]; // Add new URL for every new solution
+
+const nosql = "http://localhost:3000/#/";
+const aws = "http://localhost:3001/#/";
+const firebase = "http://localhost:3002/#/";
+let services = [nosql, aws, firebase]; // Add new URL for every new solution
 let invocations = [0, 0, 0]; // Add new item for every new solution
+
 let nrOfInvocationsPerService = 2;
 const nrOfLoops = nrOfInvocationsPerService * services.length;
 let activeService;
-let baseResult = [];
-let firebaseResult = [];
+
+let nosqlResult = [];
 let awsResult = [];
+let firebaseResult = [];
+
+const timestamp = new Date(
+  new Date().toString().split("GMT")[0] + " UTC"
+).toISOString();
 // Add new result array for every new solution
 
 (async () => {
@@ -51,15 +56,15 @@ const goToRandomService = (page) => {
 
 const pushToResultArrayAndIncreaseInvocationCount = (times) => {
   let serviceIndex;
-  if (activeService === "http://localhost:3001/#/") {
+  if (activeService === "http://localhost:3000/#/") {
     serviceIndex = 0;
-    baseResult.push(times);
-  } else if (activeService === "http://localhost:3002/#/") {
+    nosqlResult.push(times);
+  } else if (activeService === "http://localhost:3001/#/") {
     serviceIndex = 1;
-    firebaseResult.push(times);
-  } else if (activeService === "http://localhost:3003/#/") {
-    serviceIndex = 2;
     awsResult.push(times);
+  } else if (activeService === "http://localhost:3002/#/") {
+    serviceIndex = 2;
+    firebaseResult.push(times);
   } // Add new else if for every new solution
   invocations[serviceIndex]++;
   if (invocations[serviceIndex] >= nrOfInvocationsPerService)
@@ -68,20 +73,20 @@ const pushToResultArrayAndIncreaseInvocationCount = (times) => {
 };
 
 const writeResultsToJsonFiles = () => {
-  let baseJson = JSON.stringify(baseResult);
-  let firebaseJson = JSON.stringify(firebaseResult);
+  let nosqlJson = JSON.stringify(nosqlResult);
   let awsJson = JSON.stringify(awsResult);
+  let firebaseJson = JSON.stringify(firebaseResult);
   // Add new result -> json for every new solution
 
-  fs.writeFile("./base/baseResult.json", baseJson, (e) => {
-    if (e) throw e;
-  });
-
-  fs.writeFile("./firebase/firebaseResult.json", firebaseJson, (e) => {
+  fs.writeFile("./nosql/nosqlResult.json", nosqlJson, (e) => {
     if (e) throw e;
   });
 
   fs.writeFile("./aws/awsResult.json", awsJson, (e) => {
+    if (e) throw e;
+  });
+
+  fs.writeFile("./firebase/firebaseResult.json", firebaseJson, (e) => {
     if (e) throw e;
   });
   // Add new write result to file for every new solution
