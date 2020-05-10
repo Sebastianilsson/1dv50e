ec17fe367b43a6bbcecc8a0301c3d23ecb2eb99e
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2020-05-10 11:35:56 +0200

    working nosql login

--- experiment-tva/nosql/src/app/authSolution.js
+++ experiment-tva/nosql/src/app/authSolution.js
@@ -1,15 +1,26 @@
-export const userLogin = (email, password) => {
-  window.callToLogin = performance.now();
-  console.log(`Hej ${email}, du har lösenord: ${password}`);
-  window.loginDone = performance.now();
-  return {
-    id: "hejhej",
-    isAuthenticated: true,
-    email: "test@test.se",
-  };
+import axios from "axios";
+
+export const userLogin = async (email, password) => {
+  try {
+    let data = JSON.stringify({ email, password });
+    window.callToLogin = performance.now();
+    let res = await axios.post(
+      `${process.env.REACT_APP_SERVER_URL}/login`,
+      data,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    window.loginDone = performance.now();
+    return { email: res.data.email, id: res.data.id, isAuthenticated: true };
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-export const userLogout = () => {
-  console.log("Loggar ut");
+export const userLogout = async () => {
+  await axios.get(`${process.env.REACT_APP_SERVER_URL}/logout`);
   return null;
 };
