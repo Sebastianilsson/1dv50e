29e99b092d8f17c6efe9d0adaf8ff582d69b885e
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2020-05-10 17:55:19 +0200

    updating base application

--- experiment-tva/basapplikation/src/App.js
+++ experiment-tva/basapplikation/src/App.js
@@ -1,10 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Login from "./features/user/Login";
 import PrivateUserPage from "./features/user/PrivateUserPage";
-
-// TODO tried to dynamically import PrivateUserPage in useEffect didn't word since it either got imported on every page render or were set to null on second render
 
 function App() {
   return (
