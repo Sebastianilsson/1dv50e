76e03c6d3de793f85e5e3fb3e58e8a4bb2c78d24
Sebastian Nilsson <nilsson.sebastian@ymail.com>
2020-05-14 12:44:13 +0200

    updating preStudy script so that it doesnt use as much memory and results from preStudy

--- experiment-tva/puppeteerScript/preStudy.js
+++ experiment-tva/puppeteerScript/preStudy.js
@@ -11,13 +11,15 @@ let awsResult = [];
 let firebaseResult = [];
 
 const twentyFourHours = 1000 * 60 * 60 * 24;
+let invocations = 0;
 
 const startTime = Date.now();
 
 (() => {
   let timer = setInterval(async () => {
+    invocations++;
     services = shuffleArray(services);
-    await services.forEach(async (service) => {
+    services.forEach(async (service) => {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
 
@@ -34,17 +36,22 @@ const startTime = Date.now();
       await page.waitForNavigation();
 
       let times = await page.evaluate(() => {
-        return {
+        const times = {
+          timestamp: timestamp,
           firstRender: firstRender.toFixed(0),
           loginClick: loginClick.toFixed(0),
           callToLogin: callToLogin.toFixed(0),
           loginDone: loginDone.toFixed(0),
           privatePageRendered: privatePageRendered.toFixed(0),
         };
+        delete window.timestamp;
+        delete window.firstRender;
+        delete window.loginClick;
+        delete window.callToLogin;
+        delete window.loginDone;
+        delete window.privatePageRendered;
+        return times;
       });
-
-      console.log(times);
-
       pushToResultArrayAndIncreaseInvocationCount(times, service);
 
       await browser.close();
@@ -57,6 +64,8 @@ const startTime = Date.now();
         await writeResultsToJsonFiles();
       }, 20000);
     }
+
+    if (invocations % 100 === 0) writeResultsToJsonFiles();
   }, 30000);
 })();
 
@@ -89,18 +98,22 @@ const writeResultsToJsonFiles = () => {
   let firebaseJson = JSON.stringify(firebaseResult);
   // Add new result -> json for every new solution
 
-  fs.writeFile(`./nosql/nosqlPreStudy.json`, nosqlJson, (e) => {
+  fs.appendFile(`./nosql/nosqlPreStudy.json`, nosqlJson, (e) => {
     if (e) throw e;
   });
 
-  fs.writeFile(`./aws/awsPreStudy.json`, awsJson, (e) => {
+  fs.appendFile(`./aws/awsPreStudy.json`, awsJson, (e) => {
     if (e) throw e;
   });
 
-  fs.writeFile(`./firebase/firebasePreStudy.json`, firebaseJson, (e) => {
+  fs.appendFile(`./firebase/firebasePreStudy.json`, firebaseJson, (e) => {
     if (e) throw e;
   });
   // Add new write result to file for every new solution
+
+  nosqlResult = [];
+  awsResult = [];
+  firebaseResult = [];
 
   console.log("data is written to files");
 };
